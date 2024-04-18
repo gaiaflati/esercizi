ecc96aa2dc7bf265c0ac9bac9c1ad4f8202e9dd1
gaiaflati <gaiaflati@live.it>
2024-04-18 13:29:49 +0200

    es finito

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -50,3 +50,12 @@ names=["mario","matteo","davide","giulia"]
 
 for i in names:
     print(i)
+
+#3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name, print a message to them.
+#The text of each message should be the same, but each message should be personalized with the person’s name.
+
+
+for i in names:
+    print("ciao,",i, "sei bello")
+
+
