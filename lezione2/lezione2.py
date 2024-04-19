3ae55a5cd947fbf00e9550bd30990dcd47181039
gaiaflati <gaiaflati@live.it>
2024-04-19 13:33:43 +0200

    esercizio finito

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -60,6 +60,7 @@ for i in names:
 
 #3-3. Your Own List: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. 
 #Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
 transports: list=["audi","ford","bmw","fiat"]
 for i in transports:
     print("vorrei comprare un'auto",i)
@@ -68,5 +69,29 @@ for i in transports:
 #3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? 
 #Make a list that includes at least three people you’d like to invite to dinner. 
 #Then use your list to print a message to each person, inviting them to dinner.
+
 guest_list: list=["Alessandro Borghi","Leonardo Di Caprio","Beyonce"]
+for i in guest_list:
+    print(f"ciao, {i} vuoi venire a cena con me?")
+
+#3-5. Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations.
+# You’ll have to think of someone else to invite.
+#• Start with your program from Exercise 3-4. Add a print() call at the end of your program, stating the name of the guest who can’t make it.
+#• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+#• Print a second set of invitation messages, one for each person who is still in your list.
+
+
+guest_list: list=["Alessandro Borghi","Leonardo Di Caprio","Beyonce"]
+for i in guest_list:
+    print(f"ciao, {i} vuoi venire a cena con me?")
+print(f"{i[1]} non può venire alla cena")
+guest_list[1]= "Van Gogh"
+for i in guest_list:
+    print (f"{i}, ti andrebbe di cenare con me?")
+
+
+
+
+
+
 
