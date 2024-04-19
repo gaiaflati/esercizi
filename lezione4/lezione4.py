72f4e1b031dd4e8781188321201018a8316b9efd
gaiaflati <gaiaflati@live.it>
2024-04-19 12:00:15 +0200

    es fatto

---
+++ lezione4/lezione4.py
@@ -0,0 +1,9 @@
+#try to define a function named substract ouselves:
+#it should take 2 parametres
+#inside the function, it should substract the two
+#then return the rsult
+#After you defined it, call the function with some argument
+
+def substract(x,y):
+    return x-y
+print(substract(4,7))
