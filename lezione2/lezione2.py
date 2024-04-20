246736b48a81e57a93c9623c0298b3341c2527fb
gaiaflati <gaiaflati@live.it>
2024-04-20 22:29:58 +0200

    es finito

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -105,7 +105,34 @@ guest_list.append("topolino")
 for i in guest_list:
     print(f"ciao, {i} abbiamo cambiato posto per la cena, ci vediamo alle 20")
 
+#3-7. Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner,
+#and now you have space for only two guests.
+#• Start with your program from Exercise 3-6.
+# Add a new line that prints a message saying that you can invite only two people for dinner.
+#• Use pop() to remove guests from your list one at a time until only two names remain in your list. 
+#Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+#• Print a message to each of the two people still on your list, letting them know they’re still invited.
+#• Use del to remove the last two names from your list, so you have an empty list.
+# Print your list to make sure you actually have an empty list at the end of your program.
 
+print("posso invitare solo due persone")
+
+print(f"sono davvero dispiaciuta,",{guest_list[0]}," ma non posso più invitarti alla cena")
+guest_list.pop(0)
+
+print(f"sono davvero dispiaciuta,",{guest_list[1]}," ma non posso più invitarti alla cena")
+guest_list.pop(1)
+
+print(f"sono davvero dispiaciuta,",{guest_list[2]}," ma non posso più invitarti alla cena")
+guest_list.pop(2)
+
+print(f"sono davvero dispiaciuta,",{guest_list[-1]}," ma non posso più invitarti alla cena")
+guest_list.pop(-1)
+
+for i in guest_list:
+    print ("ciao,",i,"sei ancora invitato alla cena")
+del guest_list[:]
+print(guest_list)
 
 
 
