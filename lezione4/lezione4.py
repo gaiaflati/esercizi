69aa228468667ca4b04c9bc89759b678416bed08
gaiaflati <gaiaflati@live.it>
2024-04-20 17:30:45 +0200

    es finito

--- lezione4/lezione4.py
+++ lezione4/lezione4.py
@@ -47,3 +47,44 @@ def print_numbers(list:list):
     for i in list:
         print(i)
 print_numbers([2,5,7,3,8,10,4])
+
+#Write a function check_each(), which takes a list of numbers as argument.
+#Using a for loop, iterate through the list.
+#For each number, print “bigger” if it’s bigger than 5, “smaller” if it’s smaller than 5,
+#and “equal” if it’s equal to 5.
+
+def check_each(numb: list):
+    for i in numb:
+        if i>5:
+            print("bigger")
+        elif i<5:
+            print("smaller")
+        else:
+            print("equal")
+out=check_each([3,8,6,5,9,1])
+
+#Write a function add_one(). It takes an integer as argument. The function adds 1 to
+#the integer and returns it.
+#Write another function add_one_to_list(). It takes a list of integers as argument.
+#Define a variable new_list in this function.
+#Using a for loop, iterate through the argument list.
+#Using add_one(), fill new_list with integers from the argument list incremented by 1.
+#Print new_list.
+#Example:
+#add_one_to_list([1, 2, 3])
+#>>> [2, 3, 4]
+
+#prima funzione
+def add_one(a: int):
+    a +=1
+    return a
+print(add_one(3))
+
+#seconda funzione con argomento lista di interi
+def add_one_to_list (integers_list):
+    new_list=[]
+    for n in integers_list:
+        new_list.append(add_one(n))
+    print (new_list)
+add_one_to_list([4,6,1,9])
+
