2826fd62ec40a984b39b3accbb497245ce11f41a
gaiaflati <gaiaflati@live.it>
2024-05-14 13:40:46 +0200

    zoo da finire

--- lezione6/zoo.py
+++ lezione6/zoo.py
@@ -17,6 +17,15 @@ pulire i recinti e svolgere altri compiti nel nostro zoo virtuale.
 consente al guardiano dello zoo di aggiungere un nuovo animale allo zoo.
 L'animale deve essere collocato in un recinto adeguato in base alle esigenze del suo habitat
 e se c'è ancora spazio nel recinto, ovvero se l'area del recinto è ancora sufficiente per ospitare questo animale.
+
+2. remove_animal(animal: Animal, fence: Fence) (Rimuovi animale): consente al guardiano dello zoo di rimuovere un animale dallo zoo. 
+L'animale deve essere allontanato dal suo recinto. Nota bene: L'area del recinto deve essere ripristinata dello spazio che l'animale rimosso occupava.
+
+3. feed(animal: Animal) (Dai da mangiare agli animali): implementa un metodo che consenta al guardiano dello zoo di nutrire tutti gli animali dello zoo. 
+Ogni volta che un animale viene nutrito, la sua salute incrementa di 1% rispetto alla sua salute corrente, 
+ma le dimensioni dell'animale (height e width) vengono incrementate del 2%. Perciò, l'animale si può nutrire soltanto se il recinto 
+ha ancora spazio a sufficienza per ospitare l'animale ingrandito dal cibo.
+
 """
 
 
@@ -29,13 +38,16 @@ class Animal:
         self.width=width
         self.preferred_habitat=preferred_habitat
         self.health=round(100* (1/age), 3)
-        self.area_animal=height*width #inserire i get
+        self.area_animal=round(height*width, 3) #inserire i get
+        self.fence=None
 
 class Fence:
     def __init__(self, area: int, temperature: int, habitat: str) -> None:
         self.area=area
         self.temperature=temperature
         self.habitat=habitat
+        self.list_animals=[]
+        self.area_rimanente=area
 
 class Zookeper:
     def __init__(self, name: str, surname: str, id: str) -> None:
@@ -43,13 +55,27 @@ class Zookeper:
         self.surnmae=surname
         self.id=id
     
-    def add_animal(self, animal: Animal, fence: list):
-        fence: list=[]
+    def add_animal(self, animal: Animal, fence: Fence):
         if animal and isinstance(animal, Animal) and animal not in fence:
-            if Animal.area_animal<= Fence.area and Animal.preferred_habitat==Fence.habitat:   #richiamare con i get
-                fence.append(animal)
-                Fence.area-=Animal.area_animal
+            if animal.area_animal<= fence.area and animal.preferred_habitat==fence.habitat:   #richiamare con i get
+                fence.list_animals.append(animal)
+                fence.area_rimanente = fence.area-animal.area_animal
+                animal.fence=fence
+    
+    def remove_animal(animal: Animal, fence: Fence):
+        if animal and isinstance(animal, Animal) and animal in fence:
+             fence.list_animals.remove(animal)
+             fence.area+=animal.area_animal
+
+    def feed(animal: Animal):
+        animal.height+= round((animal.height*2/100), 3)
+        animal.width+= round((animal.width*2/100), 3)
+        if (animal.height*animal.width) <= animal.fence:
+            animal.health+= round((animal.health*1)/100, 3)
             
+
+
+
 
 
         
