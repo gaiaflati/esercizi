91a9a775600e7c43b069148e2648c4533ccafde0
gaiaflati <gaiaflati@live.it>
2024-05-29 11:00:15 +0200

    prova

---
+++ lezione10/lezione10.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+class AbcAnimal(ABC):
+
+    @abstractmethod
+    def verso():
+
+        pass
+
+class Cane(AbcAnimal):
+
+    def __init__(self, nome: str) -> None:
+        super().__init__()
+
+        self.nome= nome 
+
+    def verso(self):
+        return print("Bau!")
+    
+cane__1: Cane = Cane(nome="Pluto")
+cane__1.verso()
+
+class Gatto(AbcAnimal):
+    
+    def __init__(self, nome: str) -> None:
+        super().__init__()
+        self.nome=nome
+    
+    def verso(self):
+        return print("Miao!")
+
+gatto: Gatto= Gatto(nome="micio")
+gatto.verso()
